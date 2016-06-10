c28fb8139be66b788c3956b6cad6be05665cd7fc
Jonathan Foote <jf@jonathanfoote.com>
2016-06-10 14:32:08 -0400

    Initially setting up linking from ranked list

--- js/rank.js
+++ js/rank.js
@@ -1,5 +1,22 @@
 var candidates = {
+	areasUrl: 'https://raw.githubusercontent.com/promise-zones/promise-zones/master/areas.json',
 	data: [],
+
+	tpl: null,
+
+	init: function () {
+		// Populate the template used for each ranked item
+		this.tpl = $('#empty').html();
+		var self = this;
+		$.getJSON(
+			this.areasUrl,
+			function (data) {
+				self.data = data;
+				self.rank();
+			}
+		);
+	},
+
 	rank: function() {
 		var xWeight = $('#xWeight').val();
 		var yWeight = $('#yWeight').val();
@@ -18,6 +35,7 @@ var candidates = {
 		});
 		$('#candidates ol').empty();
 		var soFar = 0;
+		var visLink = '', buildLink = '';
 		// now list out the top 50 or so
 		for (var i = 0; soFar < count; i++) {
 			if (i > this.data.length) {
@@ -33,13 +51,28 @@ var candidates = {
 			// @todo: Maybe figure out way to color these on the chart dynamically (so when weights change, it
 			// updates)
 			var liClass = d.pz ? 'pz' : 'not-pz';
-			$('#candidates ol').append($(
-				'<li class="' + liClass + '">'+
-					d.area_name +
-					'<br>Unemployment: ' + candidates.percentFormat(d.ue_rate) +
-					' Poverty: ' + candidates.percentFormat(d.pv_rate) +
-				'</li>'
-			));
+			var entry = $('<li>' + this.tpl + '</li>');
+			entry.find('.area-name').text(d.area_name);
+			entry.find('.ue-rate').text(this.percentFormat(d.ue_rate));
+			entry.find('.pv-rate').text(this.percentFormat(d.pv_rate));
+			entry.find('.pop').text(this.numberFormat(d.pop));
+			if (d.fips_id) {
+				if (!visLink) {
+					// set it first time in loop
+					visLink = entry.find('.vis-link').attr('href');
+					buildLink = entry.find('.build-link').attr('href');
+					console.log('visLink', visLink);
+				}
+				entry.find('.vis-link').prop('href', visLink + d.fips_id);
+				// TODO: update build link
+			} else {
+				// Cannot link if do not have fips_id so hide links
+				entry.find('.links').hide();
+			}
+			// show or hide the pz text
+			entry.find('.pz')[d.pz ? 'show' : 'hide']();
+
+			$('#candidates ol').append(entry);
 			soFar++;
 		}
 	},
@@ -48,17 +81,16 @@ var candidates = {
 		return (Math.round(amount * 1000) / 10) + '%';
 	},
 
+	numberFormat: function (amount) {
+		amount = '' + amount;
+		return amount.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+	},
+
 	toggleSettings: function () {
 		$('#settings, #settingsToggle').toggle();
 	}
 };
 
 $(function () {
-	$.getJSON(
-		'https://raw.githubusercontent.com/promise-zones/promise-zones/master/areas.json',
-		function (data) {
-			candidates.data = data;
-			candidates.rank();
-		}
-	);
+	candidates.init();
 });
